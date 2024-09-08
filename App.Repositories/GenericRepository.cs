d2f3ad81743e6c94763024c8e50215e4438b83e3
Samed Kütahyalı <smdkthyli@gmail.com>
2024-09-08 13:54:36 +0300

    Add Generic Repository design pattern

---
+++ App.Repositories/GenericRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace App.Repositories
+{
+    public class GenericRepository<T>(AppDbContext context) : IGenericRepository<T> where T : class
+    {
+        private readonly DbSet<T> _dbSet = context.Set<T>();
+
+        public IQueryable<T> GetAll() => _dbSet.AsQueryable().AsNoTracking();
+
+        public IQueryable<T> Where(Expression<Func<T, bool>> predicate) => _dbSet.Where(predicate).AsNoTracking();
+
+        public async ValueTask<T?> GetByIdAsync(int id) => await _dbSet.FindAsync(id);
+
+        public async ValueTask AddAsync(T entity) => await _dbSet.AddAsync(entity);
+
+        public void Update(T entity) => _dbSet.Update(entity);  
+
+        public void Delete(T entity) => _dbSet.Remove(entity);
+    }
+}
