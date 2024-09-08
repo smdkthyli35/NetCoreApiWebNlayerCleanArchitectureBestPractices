c7f0691db7e89cd7a209d7a8100f505a4dc36319
Samed Kütahyalı <smdkthyli@gmail.com>
2024-09-08 11:25:00 +0300

    Add project files.

---
+++ App.API/Program.cs
@@ -0,0 +1,27 @@
+using App.Repositories;
+using App.Repositories.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddRepositories(builder.Configuration);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ App.Repositories/Extensions/RepositoryExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace App.Repositories.Extensions
+{
+    public static class RepositoryExtensions
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                ConnectionStringOption? connectionStrings = configuration.GetSection(ConnectionStringOption.Key).Get<ConnectionStringOption>();
+
+                options.UseSqlServer(connectionStrings!.SqlServer, sqlServerOptionsAction =>
+                {
+                    sqlServerOptionsAction.MigrationsAssembly(typeof(RepositoryAssembly).Assembly.FullName);
+                });
+            });
+
+            return services;
+        }
+    }
+}
